c4c07e200693ecc87152986140ae176e63336eb1
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2021-12-23 20:08:12 +0330

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // write your code here
+        System.out.println("سلام ایرانی . به جاوا خوش اومدی :)");
+//        boolean a = true;
+//        boolean b = false;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        boolean trueBoolean = true;
+//        boolean falseBoolean = false;
+//        System.out.println(trueBoolean || falseBoolean);
+//        System.out.println(trueBoolean && falseBoolean);
+
+//        System.out.println(trueBoolean ^ falseBoolean);
+//        System.out.println(true ^ true);
+
+//        System.out.println(!trueBoolean);
+//        System.out.println(!falseBoolean);
+
+
+//        String text = "Hello Bani";
+//        System.out.println(text.length());
+//        System.out.println(text.indexOf("e"));
+//        System.out.println(text.toLowerCase());
+//        System.out.println(text.toUpperCase());
+//        System.out.println(text.concat("fatemi"));
+//        System.out.println(text.replace("Bani", "SMABF"));
+//        System.out.println("Hello Hello".replace("Hello", "Bye"));
+//        System.out.println("Hello Hello".replaceFirst("Hello", "Bye"));
+//        System.out.println(text.split(" ")[0]);
+//        String[] names = text.split(" ");
+//        System.out.println(names.length);
+//        System.out.println("\\\\Hello");
+    }
+}
