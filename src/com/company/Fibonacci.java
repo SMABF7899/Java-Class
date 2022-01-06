ae8722c305fb3ada14a5e569505724f97acaeb49
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2022-01-06 17:05:15 +0330

    Edit Fibonacci.java

--- src/com/company/Fibonacci.java
+++ src/com/company/Fibonacci.java
@@ -10,7 +10,6 @@ public class Fibonacci {
         long sum = 0, num1 = 0, num2 = 1, num3 = 0;
         while (num3 < 4000000) {
             num3 = num1 + num2;
-            //System.out.println(z);
             if (num3 % 2 == 0)
                 sum += num3;
             num1 = num2;
