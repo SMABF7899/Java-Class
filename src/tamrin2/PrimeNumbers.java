0931123bb783ce96c95f5ee56d07e6907f1dc883
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2022-01-07 23:49:09 +0330

    Edit PrimeNumbers.java

--- src/tamrin2/PrimeNumbers.java
+++ src/tamrin2/PrimeNumbers.java
@@ -16,7 +16,6 @@ public class PrimeNumbers {
                 primes.add(i);
         }
         System.out.println(primes);
-        System.out.println(isPrime(4));
     }
     public static boolean isPrime(int n) {
         for (int i = 2; i <= n / 2; i++) {
