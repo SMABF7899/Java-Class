21698fa23359ea351dcd6d3f05ae3f43a406e86b
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2022-01-07 23:42:55 +0330

    Add print max member of array list

--- src/com/company/LargestPrime.java
+++ src/com/company/LargestPrime.java
@@ -3,6 +3,7 @@ package com.company;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class LargestPrime {
@@ -10,17 +11,19 @@ public class LargestPrime {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter n : ");
         long n = input.nextLong();
+        ArrayList<Long> primes = new ArrayList<>();
         while (n % 2 == 0) {
-            System.out.print(2 + " ");
+            primes.add(2L);
             n /= 2;
         }
         for (int i = 3; i <= Math.sqrt(n); i+=2) {
             while (n % i == 0) {
-                System.out.print(i + " ");
+                primes.add((long) i);
                 n /= i;
             }
         }
         if (n > 2)
-            System.out.print(n);
+            primes.add(n);
+        System.out.println(Collections.max(primes));
     }
 }
