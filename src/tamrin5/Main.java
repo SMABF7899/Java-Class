b4023b06c7a97649e24488e473b3a139fffd4f5d
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2022-02-10 09:20:45 +0330

    Solve Exercise No. 5

---
+++ src/tamrin5/Main.java
@@ -0,0 +1,60 @@
+package tamrin5;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Soldier[] soldiers = new Soldier[15];
+        ArrayList<String> ranks = new ArrayList<>();
+        ranks.add(0, "Lieutenant");
+        ranks.add(1, "Sergent");
+        ranks.add(2, "Corporal");
+        int n = 1;
+        for (int i = 0; i < 15; i++)
+            soldiers[i] = new Soldier(i + 1);
+        int numberOfCorporals = 1;
+        do {
+            for (int i = 0; i < 15; i++) {
+                int[] bullets = new int[4];
+                for (int j = 0; j < 4; j++)
+                    bullets[j] = new Random().nextInt(11);
+                soldiers[i].setBullet(bullets, ranks.get(new Random().nextInt(3)));
+            }
+
+            for (int i = 0; i < 15; i++) {
+                int finalScore = 0;
+                for (int j = 0; j < 4; j++) {
+                    finalScore += soldiers[i].getBullet()[j];
+                    soldiers[i].setFinalScore(finalScore);
+                }
+                if (soldiers[i].getFinalScore() >= 28 && soldiers[i].getInitialRank() != "Corporal")
+                    soldiers[i].setFinalRank(ranks.get(ranks.indexOf(soldiers[i].getInitialRank()) + 1));
+                else {
+                    if (soldiers[i].getInitialRank() != "Lieutenant")
+                        soldiers[i].setFinalRank(ranks.get(ranks.indexOf(soldiers[i].getInitialRank()) - 1));
+                    else
+                        soldiers[i].setFinalRank(soldiers[i].getInitialRank());
+                }
+            }
+
+            System.out.println("Shooting Test " + n);
+            System.out.println("*******************************");
+            for (int i = 0; i < 15; i++) {
+                System.out.print("Soldier ID : " + soldiers[i].getId() + "\n");
+                for (int j = 0; j < 4; j++)
+                    System.out.print("Bullet " + j + " : " + soldiers[i].getBullet()[j] + "\n");
+                System.out.print("Final Score : " + soldiers[i].getFinalScore() + "\nInitial Rank : " + soldiers[i].getInitialRank() + "\nFinal Rank : " + soldiers[i].getFinalRank() + "\n*************************************\n");
+            }
+            for (int i = 0; i < 15; i++) {
+                if(soldiers[i].getFinalRank() == "Corporal")
+                    numberOfCorporals += 1;
+            }
+            System.out.println("Shooting test " + n + " result: " + Soldier.personOfCorporals(numberOfCorporals) + "% of soldiers are Corporals.");
+            for (int i = 0; i < 15; i++) {
+                soldiers[i].setInitialRank(soldiers[i].getFinalRank());
+            }
+            n ++;
+        }while (Soldier.personOfCorporals(numberOfCorporals) > 60);
+    }
+}
---
+++ src/tamrin5/Soldier.java
@@ -0,0 +1,54 @@
+package tamrin5;
+
+public class Soldier {
+    private final int id;
+    private int[] bullet = new int[4];
+    private String initialRank;
+    private String finalRank;
+    private int finalScore;
+
+    Soldier(int id){
+        this.id = id;
+    }
+
+    public void setBullet(int[] bullet, String initialRank) {
+        this.bullet = bullet;
+        this.initialRank = initialRank;
+    }
+
+    public void setFinalRank(String finalRank) {
+        this.finalRank = finalRank;
+    }
+
+    public void setFinalScore(int finalScore) {
+        this.finalScore = finalScore;
+    }
+
+    public void setInitialRank(String initialRank) {
+        this.initialRank = initialRank;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int[] getBullet() {
+        return bullet;
+    }
+
+    public String getInitialRank() {
+        return initialRank;
+    }
+
+    public String getFinalRank() {
+        return finalRank;
+    }
+
+    public int getFinalScore() {
+        return finalScore;
+    }
+
+    public static float personOfCorporals(int n) {
+        return (n / 15f) * 100;
+    }
+}
